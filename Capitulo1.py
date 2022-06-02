dacd9f799107ff0544a394fe308ead0bc8ef0b23
ArrigoValencio <arrigo.eco@gmail.com>
2022-06-02 17:17:41 -0300

    Capítulo 1, programando o primeiro algorítmo: Pesquisa-Binária.

---
+++ Capitulo1.py
@@ -0,0 +1,38 @@
+
+    # Algorítmo Pesquisa-Binária
+def pesquisa_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+
+    while baixo <= alto:
+        meio = (baixo + alto)//2
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+
+    return None
+
+    # Testando o algorítmo Pesquisa-Binária
+# minha_lista = [1, 3, 5, 7, 9]
+#
+# print(pesquisa_binaria(minha_lista, 3))
+# print(pesquisa_binaria(minha_lista, -1))
+
+    # Determinando tempo de execução para o algorítmo Pesquisa_Binparia com n = 100 (notação 'Big O')
+minha_lista2 = range(1, 101)
+n = len(minha_lista2)
+
+import math
+tempo_execucao = math.log2(n)
+print(tempo_execucao)
+
+
+
+
+
+
+
---
+++ README.txt
@@ -0,0 +1,5 @@
+
+Este repositório contém estudos a partir do livro 'Entendendo Algorítmos',
+de autoria de Aditya Y. Bhargava. Especificamente, escreve-se os códigos 
+dos algorítmos apresentados a cada capítulo, a título de exercitar o aprendizado.
+A linguagem de programação é a mesma utilizada no livro pelo autor: Python.
