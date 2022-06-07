21e8c2c0a16a901db1fa01c5c8fbba106afdb13e
ArrigoValencio <arrigo.eco@gmail.com>
2022-06-07 15:10:23 -0300

    Atualização no arquivo dos estudos do Capítulo 2, criação dos estudos do Capítulo 3.

---
+++ Capitulo3.py
@@ -0,0 +1,25 @@
+
+    # Exemplificação de recursão (função recursiva)
+# Contagem regressiva
+def contagem_regressiva(x):
+    print(x)
+    # caso base
+    if x <= 1:
+        return
+    # caso recursivo
+    else:
+        contagem_regressiva(x - 1)
+
+print(contagem_regressiva(5))
+
+# cálculo fatorial de um número
+def fatorial(y):
+    # caso base
+    if y == 1:
+        return 1
+    # caso recursivo
+    else:
+        return y * fatorial(y-1)
+
+print(fatorial(3))
+
