8d33a55d3da97576c67621b5055060cba97ccf5b
ArrigoValencio <arrigo.eco@gmail.com>
2022-06-14 15:47:00 -0300

    Continuação nos estudos do Capítulo 6, implementando o algoritmo de pesquisa em largura.

--- Capitulo6.py
+++ Capitulo6.py
@@ -11,3 +11,30 @@ meu_grafo['Anuj'] = []
 meu_grafo['Peggy'] = []
 meu_grafo['Jonny'] = []
 meu_grafo['Thom'] = []
+
+    # Algoritmo Pesquisa em largura
+# Implementando o algoritmo a partir do grafo criado (busca-se um vendedor de mangas na rede de amigos)
+from collections import deque
+def pesquisa_largura(pessoa):
+    # Criando fila
+    fila_pesquisa = deque()
+    fila_pesquisa += meu_grafo[pessoa]
+
+    # Criando função que defina vendedor de mangas (no caso, se a última letra do nome da pessoa é 'm')
+    def e_vendedor_mangas(pessoa_da_rede):
+        return pessoa_da_rede[-1] == 'm'
+    pessoas_verificadas = []
+    while fila_pesquisa:
+        pessoa_da_rede = fila_pesquisa.popleft()
+        if not pessoa_da_rede in pessoas_verificadas:
+            if e_vendedor_mangas(pessoa_da_rede):
+                return print(pessoa_da_rede + ' vende mangas.')
+            else:
+                fila_pesquisa += meu_grafo[pessoa_da_rede]
+                pessoas_verificadas.append(pessoa_da_rede)
+    return print('Ninguém vende mangas.')
+
+# Testando o algoritmo pesquisa em largura
+pesquisa_largura('eu')
+
+
